5965003e60e7b1cc6116cd3575fac4e536aaa3a5
Noah Clark <Clarknoah@live.com>
2026-02-16 17:13:26 -0600

    added error handeling, did some miner bug fixes and updated the display screen, project complete

--- src/features/check-in-out/TodayActivity.jsx
+++ src/features/check-in-out/TodayActivity.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 import Heading from "../../ui/Heading";
 import Row from "../../ui/Row";
+import TodayItem from "./TodayItem";
+import Spinner from "../../ui/Spinner";
+import { useTodayActivity } from "./useTodayActivity";
 
 const StyledToday = styled.div`
   /* Box */
@@ -36,14 +39,29 @@ const NoActivity = styled.p`
   margin-top: 0.8rem;
 `;
 
-function Today() {
+function TodayActivity() {
+  const { stays, isLoading } = useTodayActivity();
+
   return (
     <StyledToday>
       <Row type="horizontal">
         <Heading as="h2">Today</Heading>
       </Row>
+      {!isLoading ? (
+        stays?.length > 0 ? (
+          <TodayList>
+            {stays.map((stay) => (
+              <TodayItem key={stay.id} stay={stay} />
+            ))}
+          </TodayList>
+        ) : (
+          <NoActivity>No activity today</NoActivity>
+        )
+      ) : (
+        <Spinner />
+      )}
     </StyledToday>
   );
 }
 
-export default Today;
+export default TodayActivity;
